b1766038d379208602ed9cf5b8674c16470982b7
Sergo <78502767+Siarhei-Ihnatovich@users.noreply.github.com>
2022-07-30 11:18:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const reverse = (str) => {
+  let i = 0;
+  let result = '';
+
+  while (i < str.lenght) {
+    result = result + str[i]
+    i = i + 1;
+  }
+  return result;
+}
+console.log(reverse(str));
